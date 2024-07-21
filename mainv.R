294161165c8fa9093f27a49bd16093e00499367a
Alareny <saidoualareny@gmail.com>
2024-07-21 10:41:25 +0000

    filter from 30

--- mainv.R
+++ mainv.R
@@ -3,6 +3,9 @@ library(here)
 
 tv_hours_tablev <- gss_cat %>% 
   group_by(marital) %>% 
+  filter(age>=30) %>% 
   summarise(mean_tv_hours=mean(tvhours, na.rm=T))
 
 write.csv(tv_hours_tablev, here("TV_hours_by_Maritalv"))
+
+tv_hours_tablev

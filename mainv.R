d385cc3278b878e3327d1b06266697e6b54d8ff7
Alareny <saidoualareny@gmail.com>
2024-07-23 09:11:59 +0000

    filter to only those under 30

--- mainv.R
+++ mainv.R
@@ -3,7 +3,7 @@ library(here)
 
 tv_hours_tablev <- gss_cat %>% 
   group_by(marital) %>% 
-  filter(age>=30) %>% 
+  filter(age<30) %>% 
   summarise(mean_tv_hours=mean(tvhours, na.rm=T))
 
 write.csv(tv_hours_tablev, here("TV_hours_by_Maritalv"))

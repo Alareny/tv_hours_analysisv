4365bfb35f8515f95479c6aa8b30451b3c2adc4d
Alareny <saidoualareny@gmail.com>
2024-07-28 06:50:39 +0000

    longer names

--- mainv.R
+++ mainv.R
@@ -1,11 +1,22 @@
 library(tidyverse)
+
 library(here)
 
+<<<<<<< Updated upstream
 tv_hours_tabulation <- gss_cat %>% 
   group_by(marital) %>% 
   filter(age<30) %>% 
+=======
+
+tv_hours_tablev <- gss_cat %>% 
+  
+  group_by(marital) %>% 
+  
+  filter(age>=30) %>% 
+  
+>>>>>>> Stashed changes
   summarise(mean_tv_hours=mean(tvhours, na.rm=T))
 
 write.csv(tv_hours_tabulation, here("TV_hours_by_Maritalv"))
 
-tv_hours_tablev
+
